6797edc8475a9f1da41b76c7a5dd22d703d51129
Siddaram <siddaram.halli@gmail.com>
2025-01-28 19:26:17 +0530

    Added a default index view

--- RagQA/models.py
+++ RagQA/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Document():
+    title = models.CharField('title', max_length=255)
+    content = models.TextField(
+        'Content', blank=True, null=True, max_length=2000)
